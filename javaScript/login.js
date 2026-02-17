765c1267811ca90736fb2dcf4cf38964360524aa
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-18 03:07:14 +0530

    Login Validation Logic

---
+++ javaScript/login.js
@@ -0,0 +1,33 @@
+const loginBtn = document.getElementById("login-btn");
+
+loginBtn.addEventListener("click", function () {
+
+    // number input access with value
+    const inputNumber = document.getElementById("input-number");
+    let number = inputNumber.value.trim();
+    // console.log(number);
+
+    // pin input access with value
+    const inputPin = document.getElementById("input-pin");
+    let pin = inputPin.value.trim();
+    // console.log(pin);
+
+    // check the number & pin (true :: go the next 
+    //  page  || false :: return login failed);
+    if (!number || !pin) {
+        alert("Please fill all fields");
+        return;
+    }
+    if (number === "1234567890" && pin === "1234") {
+        alert("login Success");
+        inputNumber.value = "";
+        inputPin.value = "";
+        window.location.assign("/javaScript/home.js");
+        return;
+    } else {
+        alert("oops login failed");
+        inputPin.value = "";
+        return;
+    }
+
+})
