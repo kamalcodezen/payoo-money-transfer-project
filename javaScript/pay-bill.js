b33d34f864b4b18b0984527d33d986d2abebef0a
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-19 12:42:06 +0530

    pay bill transaction history create

--- javaScript/pay-bill.js
+++ javaScript/pay-bill.js
@@ -27,7 +27,39 @@ document.getElementById("pay-bill-btn")
         // current balance access and calculate ,validation 
         const currentBalance = getBalance();
         const newBalance = currentBalance - billPayAmount;
-    
+        if (newBalance < 0) {
+            alert("Insufficient Balance");
+            return;
+        }
+
+        // pin access and validation
+        const pin = Number(getValueFromInput("pay-bill-pin"));
+        if (!pin || isNaN(pin) || pin !== 1234) {
+            alert("Wrong Pin!");
+            return;
+        }
+
+        alert("Bill Pay Success");
+        setBalance(newBalance);
+
+        // transaction history transfer ----
+        // history div access and create new div inside and add prepend ----
+        const historyDiv = document.getElementById("history");
+        const newHistoryDiv = document.createElement("div");
+        newHistoryDiv.innerHTML = `
+         < div class="w-11/12 bg-base-100 flex    rounded-2xl mx-auto gap-4 items-center mt-2  py-2" >
+         <div>
+           <img class="w-[30px] h-[40px] " src="./assets/opt-1.png" alt="">
+         </div>
+         <p class="text-neutral/50 font-bold">
+            Money Sent to ${BillAccountNumber} <br>
+            Amount Rs.${billPayAmount}<br>
+            New Balance: Rs.${newBalance} <br>
+            Date: ${new Date().toLocaleString()}
+         </p>
+        <div/>
+        `
+        newHistoryDiv.prepend(historyDiv);
 
 
     })
